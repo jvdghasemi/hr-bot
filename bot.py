d16a9312d5bcf430737286c9152050798744aa70
jvdghasemi <jvdghasemi@gmail.com>
2026-06-19 23:10:19 +0330

    update

--- bot.py
+++ bot.py
@@ -6,11 +6,22 @@ TOKEN = os.getenv("TOKEN")
 
 keyboard = [
     ["🏢 اطلاعات شرکت", "⏰ ساعت کاری"],
-    ["🚕 ایاب و ذهاب", "📝 مرخصی"],
+    ["🚕 ایاب و ذهاب", "🌐 شبکه های اجتماعی"],
     ["💰 حقوق", "📞 تماس‌ها"]
 ]
 
 reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
+social_keyboard = ReplyKeyboardMarkup(
+    [
+        ["📷 اینستاگرام"],
+        ["✈️ تلگرام"],
+        ["🔵 لینکدین"],
+        ["🟢 بله"],
+        ["🔙 بازگشت"]
+    ],
+    resize_keyboard=True
+)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -25,16 +36,43 @@ async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     if text == "⏰ ساعت کاری":
         await update.message.reply_text("از 7 صبح تا 18 عصر")
+
     elif text == "🚕 ایاب و ذهاب":
         await update.message.reply_text("سرویس داریم 🚐")
-    elif text == "📝 مرخصی":
-        await update.message.reply_text("۲۶ روز در سال")
+
+    elif text == "🌐 شبکه های اجتماعی":
+        await update.message.reply_text(
+            "یکی از شبکه‌ها را انتخاب کنید:",
+            reply_markup=social_keyboard
+        )
+
+    elif text == "📷 اینستاگرام":
+        await update.message.reply_text("https://instagram.com/yourpage")
+
+    elif text == "✈️ تلگرام":
+        await update.message.reply_text("https://t.me/yourchannel")
+
+    elif text == "🟢 روبیکا":
+        await update.message.reply_text("https://rubika.ir/yourpage")
+
+    elif text == "🔵 بله":
+        await update.message.reply_text("https://ble.ir/yourpage")
+
+    elif text == "🔙 بازگشت":
+        await update.message.reply_text(
+            "از منو انتخاب کنید:",
+            reply_markup=reply_markup
+        )
+
     elif text == "💰 حقوق":
         await update.message.reply_text("آخر ماه واریز میشه 💵")
+
     elif text == "📞 تماس‌ها":
         await update.message.reply_text("داخلی 101")
+
     elif text == "🏢 اطلاعات شرکت":
         await update.message.reply_text("شرکت داروسازی ایران هورمون")
+
     else:
         await update.message.reply_text("از منو انتخاب کن")
 
